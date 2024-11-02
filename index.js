f870f7816cd1c1f8171f75a0320de338c1c40f63
jhraju <jhraju890@gmail.com>
2024-11-02 13:31:39 +0600

    added data create and read

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.port || 5000;
+
 
 // middewere
 app.use(cors());
@@ -29,6 +31,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db('coffeeDb').collection('coffee');
+   
+    // 2. ekhon data read korbo mane display te dekhabo----
+    app.get('/coffee', async(req, res) =>{
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+// 1. new coffee backend a pathabo it means data create---
+   app.post('/coffee', async(req, res) =>{
+    const newCoffee = req.body;
+    console.log(newCoffee);
+    const result = await coffeeCollection.insertOne(newCoffee);
+    res.send(result);
+   })  
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
